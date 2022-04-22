853b2685ab05771fd16ad66a3a771e1e34876048
derche <derche@acxiom.com>
2022-04-22 10:06:02 +0800

    add log

--- src/main/java/com/liveramp/recruitment/workflow/infrastructure/util/WorkNodeJobService.java
+++ src/main/java/com/liveramp/recruitment/workflow/infrastructure/util/WorkNodeJobService.java
@@ -1,12 +1,40 @@
 package com.liveramp.recruitment.workflow.infrastructure.util;
 
+import com.liveramp.recruitment.workflow.domain.Repository.TaskLogRepository;
+import com.liveramp.recruitment.workflow.domain.entity.TaskLog;
+import com.liveramp.recruitment.workflow.domain.entity.work.WorkReport;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by derche on 2022/4/22.
  */
 @Service
 public class WorkNodeJobService {
+
+    @Autowired
+    TaskLogRepository taskLogRepository;
+
+    public TaskLog saveLog(String taskId, WorkReport workReport){
+        TaskLog taskLog = taskLogRepository.findOneByTaskId(taskId);
+        if(taskLog == null){
+            taskLog = new TaskLog();
+            taskLog.setId(UUID.randomUUID().toString());
+            taskLog.setTaskId(taskId);
+        }
+
+        List<WorkReport> workReportList = CollectionUtils.isEmpty(taskLog.getWorkReportList())? new ArrayList<>() : taskLog.getWorkReportList();
+        workReportList.add(workReport);
+        taskLog.setWorkReportList(workReportList);
+
+        return taskLogRepository.save(taskLog);
+    }
+
     public void execute(){
 
     }
